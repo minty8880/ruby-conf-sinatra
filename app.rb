63bbdb6f6bf2a7faab4079251b12a902b6c15209
tracymu <tracy.musung@moomumedia.com>
2014-02-19 12:28:03 +1100

    Added a form to submit coffees

--- app.rb
+++ app.rb
@@ -1,6 +1,25 @@
 require "sinatra"
 require "sinatra/reloader"
 
+$coffees = []
+def template
+  """
+  <html>
+  <body>
+  <form action='/' method='get'>
+  What: <input name='what'>
+  Cost: <input name='cost'>
+  <button type='submit'>add coffee</button>
+  </form>
+
+  #{$coffees.inspect}
+
+  </body>
+  </html>
+  """
+end
+
 get "/" do
-"Hello ruby conf!"
+  $coffees << params
+  template
 end
